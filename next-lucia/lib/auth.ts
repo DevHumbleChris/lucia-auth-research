f976b9674d76bf4e528f137d22baa5cb90dcb56c
DevHumbleChris <dev.humblechris254@gmail.com>
2024-03-17 13:40:15 +0300

    added lucia configuration file

---
+++ next-lucia/lib/auth.ts
@@ -0,0 +1,92 @@
+import { Lucia } from "lucia";
+import { PrismaAdapter } from "@lucia-auth/adapter-prisma";
+import { prisma } from "./db";
+import { cookies } from "next/headers";
+import { cache } from "react";
+import { GitHub } from "arctic";
+
+import type { Session, User } from "lucia";
+
+// import { webcrypto } from "crypto";
+// globalThis.crypto = webcrypto as Crypto;
+
+const adapter = new PrismaAdapter(prisma.session, prisma.user);
+
+export const lucia = new Lucia(adapter, {
+  sessionCookie: {
+    attributes: {
+      secure: process.env.NODE_ENV === "production",
+    },
+  },
+  getUserAttributes: (attributes) => {
+    return {
+      // we don't need to expose the hashed password!
+      id: attributes.id,
+      email: attributes.email,
+      oauthAccount: attributes.oauthAccount,
+    };
+  },
+});
+
+declare module "lucia" {
+  interface Register {
+    Lucia: typeof lucia;
+    DatabaseUserAttributes: DatabaseUserAttributes;
+  }
+}
+
+interface DatabaseUserAttributes {
+  id: string;
+  email: string;
+  oauthAccount: OauthAccount;
+}
+
+interface OauthAccount {
+  providerId: string;
+  providerUserId: string;
+  userId: string;
+  userEmail: string;
+  userName: string;
+  userAvatarURL: string;
+}
+
+export const validateRequest = cache(
+  async (): Promise<
+    { user: User; session: Session } | { user: null; session: null }
+  > => {
+    const sessionId = cookies().get(lucia.sessionCookieName)?.value ?? null;
+    if (!sessionId) {
+      return {
+        user: null,
+        session: null,
+      };
+    }
+
+    const result = await lucia.validateSession(sessionId);
+    // next.js throws when you attempt to set cookie when rendering page
+    try {
+      if (result.session && result.session.fresh) {
+        const sessionCookie = lucia.createSessionCookie(result.session.id);
+        cookies().set(
+          sessionCookie.name,
+          sessionCookie.value,
+          sessionCookie.attributes
+        );
+      }
+      if (!result.session) {
+        const sessionCookie = lucia.createBlankSessionCookie();
+        cookies().set(
+          sessionCookie.name,
+          sessionCookie.value,
+          sessionCookie.attributes
+        );
+      }
+    } catch {}
+    return result;
+  }
+);
+
+export const github = new GitHub(
+  process.env.GITHUB_CLIENT_ID!,
+  process.env.GITHUB_CLIENT_SECRET!
+);
